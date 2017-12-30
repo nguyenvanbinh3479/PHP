4cd17d204759d2dacaddc6098cad68540c7f68db
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2017-12-30 20:28:03 +0700

    Fixed if-else

--- 4.Lenh-if-else.php
+++ 4.Lenh-if-else.php
@@ -4,7 +4,32 @@
     $so_can_kiem_tra = 12;
     $sodu = $so_can_kiem_tra % 2;
     if($sodu == 0){
-        echo $so_can_kiem_tra.' la so chan';
+        echo $so_can_kiem_tra.' là số chẵn <br>';
     }
+
+    $nam = 1990;
+    $kiemtra = ($nam % 4 == 0 || $nam % 400 == 0);
+    if($kiemtra % 100 != 0){
+        echo $nam.' là năm nhuận <br>';
+    }
+    else{
+        echo $nam.' không phải Là năm nhuận <br>';
+    }
+
+    $mau = 'yellow';
+    if($mau == 'xanh' || $mau == 'blue'){
+        echo 'đây là màu xanh <br>';
+    }
+    else if($mau == 'đỏ' || $mau == 'red'){
+        echo 'đây là màu đỏ <br>';
+    }
+    else if($mau == 'vàng' || $mau == 'yellow'){
+        echo 'đây là màu vàng <br>';
+    } 
+    else{
+        echo 'các màu khác <br>';
+    }
+    
+
 
 ?>
