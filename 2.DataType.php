d004ee4e6cb6e64d98bb79d621761d908949ece1
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2017-12-30 14:38:35 +0700

    Added 2.datatype

--- 2.DataType.php
+++ 2.DataType.php
@@ -5,6 +5,11 @@
 
     /* ép dữ liệu kiểu int */
     $age = "98";
+    $year = 100;
+    $count = $age + $year;
+    var_dump(is_int($age));//kiểm tra có phải kiểu int ko
+    var_dump(is_int($count));
     $age = (int)$age;
+    var_dump(is_int($age));
 
 ?>
