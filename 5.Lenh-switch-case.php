494f71eb09474609032a347332d0ec81332ef796
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2017-12-31 13:41:33 +0700

    Fixed project

--- 5.Lenh-switch-case.php
+++ 5.Lenh-switch-case.php
@@ -1,6 +1,44 @@
 <?php
 
     /* switch-case */
-    
+    $number = 5;
+    switch($number){
+        case 0:
+            echo 'số không <br>';
+            break;
+        case 1: 
+            echo 'số một <br>';
+            break;
+        case 2:
+            echo 'số hai <br>';
+            break;
+        case 3:
+            echo 'số ba <br>';
+            break;
+        case 4:
+            echo 'số bốn <br>';
+            break;
+        default:
+            echo 'không tìm thấy <br>';
+            break;
+    }
+
+    $num = 12;
+    $midle = null;
+    switch($num){
+        case 12:
+            $midle = $num % 2;
+            switch($midle){
+                case 0:
+                    echo 'số chẳn <br>';
+                    break;
+                default:
+                    echo 'số lẻ <br>';
+                    break;
+            }
+        break;
+        default:
+            break;
+    }
 
 ?>
