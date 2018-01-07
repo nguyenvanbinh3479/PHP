1234038361f16a3840166b33bc6e7f02c94935aa
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-07 13:48:39 +0700

    Fixed de-quy

--- 18.Giai-thuat-de-quy.php
+++ 18.Giai-thuat-de-quy.php
@@ -21,43 +21,68 @@
         }
         return $n2 + tinhtong2($n2 - 1);
     }
-    echo tinhtong2(100);
+    echo tinhtong2(100) . '<br>';
 
-    /* đệ quy tuyến tính */
-    //tính giá trị của phần tử thứ $n của dãy Fibonacci
-    // function fibo($n3)
+    /* đệ quy nhị phân */
+    // Hàm tính giá trị của phần tử thứ $n của dãy Fibonacci
+    // function Fibo($n)
     // {
-    //     if ($n3 <= 2)
-    //     {
+    //     if ($n <= 2){
     //         return 1;
     //     }
-    //     else
-    //     {
-    //         return (fibo($n3 - 2) + fibo($n3 -1));
+    //     else {
+    //         return (Fibo($n - 2) + Fibo($n - 1));
     //     }
     // }
-    // echo fibo(100);
+    
+    // // Truyền 100 vào để test
+    // echo Fibo(100);
 
     /* đệ quy phi tuyến */
     // Tính phần tử thứ 8 của dãy
-    // function pheptinh($n4)
-    // {
-    //     //nếu n<6 thì trả về chính nó
-    //     if ($n4 <6)
-    //     {
-    //         return $n4;
-    //     }
-    //     else
-    //     {
-    //         // Ngược lại tính tổng từ 1 tới $n - 1, và mỗi phần tử lại gọi làm hàm chính nó
-    //         $tong2 = 0; 
-    //         for ($i2 = 1; $i2 = $n4; $i2++)
-    //         {
-    //             $tong2 += pheptinh($n4 - $i2); 
-    //         }
-    //         return $tong2;
-    //     }
-    // }
-    // echo pheptinh(6);
+    function pheptinh($n4)
+    {
+        // Neeus $n < 6 thì trả về chính nó
+        if ($n4 < 6){
+            return $n4;
+        }
+        else{
+            // Ngược lại tính tổng từ 1 tới $n - 1, và mỗi phần tử lại gọi làm hàm chính nó
+            $tong4 = 0;
+            for ($i4 = 1; $i4 < $n4; $i4++){
+                $tong4 += pheptinh($n4 - $i4);
+            }
+            return $tong4;
+        }
+    }
+    
+    echo pheptinh(6) . '<br>';
+
+    /* đệ quy hổ tương*/
+    // Hàm đệ quy U
+    function U($n5)
+    {
+        if ($n5 < 5){ // điều kiện dừng
+            return $n5;
+        }
+        else{
+            return U($n5 - 1) + G($n5 - 2);
+        }
+    }
+    
+    // Hàm đệ quy G
+    function G($n5)
+    {
+        if ($n5 <= 8){ // điều kiện dừng
+            return $n5 - 3;
+        }
+        else{
+            return U($n5 - 1) + G($n5 - 2);
+        }
+    }
+    
+    // Gọi Hàm
+    
+    echo G(12);
 
 ?>
