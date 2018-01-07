89ad1d73747f2def6137d9dda5ef2083674df9ef
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-07 20:19:55 +0700

    Added require-include-lession.php

---
+++ 24.5inport.php
@@ -0,0 +1,13 @@
+<?php
+
+    function show_mess()
+    {
+        echo 'đây là hàm mess trong inport.php';
+    }
+
+    function show()
+    {
+        echo 'đây là hàm mess';
+    }
+
+?>
---
+++ 24.Lenh require - require_once - include - include_once.php
@@ -0,0 +1,21 @@
+<?php
+
+    /* require */
+    // require "/24.5inport.php";
+    // require_once "/24.5inport.php";
+    // show_mess();
+
+    // Import file 24.5inport.php
+    include "/24.5inport.php";
+    
+    // Sử dụng hàm show_message trong file 24.5inport.php
+   show();
+
+    // Import file 24.5inport.php
+    include_once "/24.5inport.php";
+    
+    // Sử dụng hàm show_message trong file 24.5inport.php
+    show();
+
+
+?>
