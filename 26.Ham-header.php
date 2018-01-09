545c4538be39ae8b3c430b9756c12f0f0383fe35
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-01-09 13:18:46 +0700

    Added header-lession

---
+++ 26.Ham-header.php
@@ -0,0 +1,12 @@
+<?php
+
+    /* điều hướng trang */
+    header('Location: https://nguyenvanbinh3479.github.io/', true, 404);
+
+    /* khắc phục lỗi font */
+    header('Content-type: text/html; charset=utf-8');
+
+    /* khai báo định dang file */
+    header("Content-type: text/javascript");
+
+?>
